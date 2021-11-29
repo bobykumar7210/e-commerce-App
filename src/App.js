d3d39f3cfe82bf96327f35fa763c07fd2d8baa9d
mohit <nvnkr1999@gmail.com>
2021-11-29 22:40:39 +0530

    adding shop page and preview-collection-itrm ,collection-item component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import React from "react";
 
 import "./App.css";
 import HomePage from "./pages/homepage/home.component";
+import ShopPage from "./pages/shop/shop.component";
 import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
 }
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 export default App;
--- src/pages/homepage/home.component.jsx
+++ src/pages/homepage/home.component.jsx
@@ -5,7 +5,7 @@ import Directory from "../../components/directory/directory.component";
 const HomePage=(props)=>{
     return(
        <div className='homepage'>
-     
+        
        <Directory/>
       </div>
     )
